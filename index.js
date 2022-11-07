6a03fe80bfa3b5c149b732112b0094168cd50103
Kevin <queben.quin@gmail.com>
2022-11-06 22:10:49 -0600

    Add function to playRound and output game results. EX: "You Lose! Paper beats rock!"

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ function getComputerChoice() {
         return 'Unknown Error!'
     }
 }
+
+
+let playerSelection = 'paper';
+
+function playRound(playerSelection, computerChoice) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if ((computerChoice === 'Rock' && playerSelection === 'scissors') ||
+        (computerChoice === 'Paper' && playerSelection === 'rock') ||
+        (computerChoice === 'Scissors' && playerSelection === 'paper')
+    ) {
+        playerSelection = (playerSelection.substr(0, 1)).toUpperCase() + playerSelection.slice(1);
+        return `You Lose! ${computerChoice} beats ${playerSelection}`
+    } else if ((playerSelection === 'rock' && computerChoice === 'Scissors') ||
+    (playerSelection === 'paper' && computerChoice === 'Rock') ||
+    (playerSelection === 'scissors' && computerChoice === 'Paper')
+    ) {
+        playerSelection = (playerSelection.substr(0, 1)).toUpperCase() + playerSelection.slice(1);
+
+        return `You Win! ${playerSelection} beats ${computerChoice}`
+    } else {
+        return 'Tie!'
+    }
+}
+
+console.log(playRound(playerSelection, getComputerChoice()));
