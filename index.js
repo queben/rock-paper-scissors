cb4b46eb200f10969e2f71ecac26d01baed0e48c
Kevin <queben.quin@gmail.com>
2022-11-06 21:09:47 -0600

    Add function to generate computer choice of rock, paper, scissors, or unknown error

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3 + 1)
+
+    if (computerChoice === 1) {
+        return 'Rock'
+    } else if (computerChoice === 2) {
+        return 'Paper'
+    } else if (computerChoice === 3) {
+        return 'Scissors'
+    } else {
+        return 'Unknown Error!'
+    }
+}
